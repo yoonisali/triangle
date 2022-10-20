14e1d182aef06753b2bdaaac8d43373965d9d363
yoonisali <yoonismahamoudali@gmail.com>
2022-10-20 15:34:06 -0700

    add css styling
    Co-authored-by: Ryan Krueger <thegckid@gmail.com>
    Co-authored-by: Bodie Wood <bodiewood@hotmail.com>
    
    add bootstrap styling
    
    Co-authored-by: Ryan Krueger <thegckid@gmail.com>
    Co-authored-by: Bodie Wood <bodiewood@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 function isItATriangle(event) {
   event.preventDefault();
 
-
   let equal = document.querySelector("div#equal");
   let isos = document.querySelector("div#iso");
   let scal = document.querySelector("div#sca");
@@ -10,18 +9,33 @@ function isItATriangle(event) {
   let length2 = parseInt(document.getElementById("length2").value);
   let length3 = parseInt(document.getElementById("length3").value);
 
+  equal.setAttribute('class', 'hidden');
+  isos.setAttribute('class', 'hidden')
+  scal.setAttribute('class', 'hidden');
+
   if (length1 === length2 && length1 == length3) {
-    window.alert('Equilateral');
+    equal.removeAttribute('class', 'hidden');
   } else if (length1 === length2 || length1 === length3 || length2 === length3) {
-    window.alert('Isosceles');
+    isos.removeAttribute('class', 'hidden');
   } else if (length1 + length2 <= length3 || length1 + length3 <= length2 || length2 + length3 <= length1) {
-    window.alert('NOT A TRIANGLE');
+    window.alert('NOT A TRIANGLE DUMMY!')
   } else if (!(length1 === length2 && length1 == length3)) {
-    window.alert('Scalene');
+    scal.removeAttribute('class', 'hidden');
   }
 }
 
 window.addEventListener("load", function () {
   const form = document.getElementById("triangle");
   form.addEventListener("submit", isItATriangle);
+
+  let body = document.body;
+  body.onkeydown = function() {
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
+};
+
+  body.onkeyup = function() {
+  body.style.backgroundColor = "darkgoldenrod";
+  body.style.color = "black";
+};
 });
